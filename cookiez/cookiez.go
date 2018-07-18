d97df72e820c86572b174fceef063d38f5d113f3
fractalbach <4bach@protonmail.com>
2018-07-18 11:23:27 -0700

    improved cookie handling and added more useful messages

--- cookiez/cookiez.go
+++ cookiez/cookiez.go
@@ -9,16 +9,38 @@ import (
 )
 
 const (
-	MainCookieName  = "yummy-cookie"
-	hashKeyLen  = 32 // can be 32 or 64 bytes
-	blockKeyLen = 16 // can be 16, 24, or 32 bytes.
+	MainCookieName = "yummy-cookie"
+	hashKeyLen     = 32 // can be 32 or 64 bytes
+	blockKeyLen    = 16 // can be 16, 24, or 32 bytes.
 )
 
 var (
-	s          = GimmeCookie()
-	exampleID  = []byte{1, 2, 3, 4, 5}
-	exampleKey = securecookie.GenerateRandomKey(32)
+	s      = GimmeCookie()
+	idIter = 123
 )
+
+type userPair struct {
+	ID     int
+	Jumble []byte
+}
+
+func newUserPair() userPair {
+	return userPair{
+		ID:     nextID(),
+		Jumble: securecookie.GenerateRandomKey(32),
+	}
+}
+
+func (u *userPair) String() string {
+	return fmt.Sprintf("id: %d, jumble: %x", u.ID, u.Jumble)
+}
+
+// increments the package's global variable "idIter", copies that new value,
+// and returns the copy.  For use as a new unique player id.
+func nextID() int {
+	idIter++
+	return idIter
+}
 
 // GimmeCookie randomly generates keys and returns a Secure Cookie.
 func GimmeCookie() *securecookie.SecureCookie {
@@ -32,10 +54,7 @@ func GimmeCookie() *securecookie.SecureCookie {
 
 // SetCookieHandler is called by the server to hand out cookies.
 func SetCookieHandler(w http.ResponseWriter, r *http.Request) {
-	value := map[string]([]byte){
-		"id":         exampleID,
-		"examplekey": exampleKey,
-	}
+	value := newUserPair()
 	encoded, err := s.Encode(MainCookieName, value)
 	if err != nil {
 		log.Println(err)
@@ -45,21 +64,29 @@ func SetCookieHandler(w http.ResponseWriter, r *http.Request) {
 		Name:  MainCookieName,
 		Value: encoded,
 		Path:  "/",
+		MaxAge: 60,
 	}
 	http.SetCookie(w, cookie)
+	fmt.Fprintln(w, "Here, have a cookie!")
 }
 
+// ReadCookieHandler checks the client's cookies, and prints back a message
+// if it's valid.  Does not check yet check to see if the id matches the
+// value that it should; simply just confirms that it is a valid cookie.
 func ReadCookieHandler(w http.ResponseWriter, r *http.Request) {
 	cookie, err1 := r.Cookie(MainCookieName)
 	if err1 != nil {
 		log.Println(err1)
 		return
 	}
-	value := make(map[string]([]byte))
+	value := userPair{}
 	err2 := s.Decode(MainCookieName, cookie.Value, &value)
 	if err2 != nil {
+		fmt.Fprintln(w, "You're a strange cookie! Here's a new one.")
 		log.Println(err2)
+		SetCookieHandler(w, r)
 		return
 	}
-	fmt.Fprintln(w, value)
+	fmt.Fprintln(w, "You've got a valid cookie: ", value)
+	fmt.Fprintln(w, "Once those 2 values are checked; you'll be logged in. ")
 }
