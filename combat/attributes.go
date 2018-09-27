d7116b4eed043de4dab1bf7dfa965269eaca79b1
fractalbach <fractalbach@gmail.com>
2018-09-26 22:07:09 -0700

    removed unused folders

--- combat/attributes.go
+++
@@ -1,9 +0,0 @@
-package combat
-
-/*
-   Attributes of an Actor include things like health and fatigue and other
-   attributes that are relevant in the context of a battle.
-*/
-type Attributes struct {
-	health int
-}
